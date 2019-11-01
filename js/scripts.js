67589b22c3af49c05b2fefa8f3ce26a172458567
Adilet Momunaliev <adilet@>
2019-11-01 15:59:17 -0700

    Trying to come up with different approch

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,73 @@ $(document).ready(function()
   {
       event.preventDefault();
       var dataFromUser =parseFloat($("#inputID").val());
-      //console.log(dataFromUser);
-      //var size=Math.sqrt(dataFromUser);
+
+      var matrix = [];
+      var index = 0;
+      var element ="";
+      while(dataFromUser>=0)
+      {
+        dataFromUser--;
+        matrix[index]=index;
+        element = index.toString();
+        if(index<10)
+        {
+
+            if(element === '1' )
+            {
+
+              matrix[index] = "Beep";
+              index++;
+            }
+            else if(element==='2')
+            {
+              matrix[index] = "Boop";
+              index++;
+            }
+            else if(element ==='3')
+            {
+              matrix[index] = "I'm sorry, Dave. I'm afraid I can't do that.";
+              index++;
+            }
+            else
+            {
+              index++;
+            }
+          }
+          /*
+          else if(index<20 && index>=10)
+          {
+            if(index === 10 )
+            {
+              matrix[index] = "Beep";
+              index++;
+            }
+            else if(index===2)
+            {
+              matrix[index] = "Boop";
+              index++;
+            }
+            else if(index ===3)
+            {
+              matrix[index] = "I'm sorry, Dave. I'm afraid I can't do that.";
+              index++;
+            }
+            else
+            {
+              index++;
+            }
+          }*/
+
+
+      }
+      console.log(matrix);
+
+
+
+
+
+
+      /*
       var row = Math.ceil(dataFromUser/10);
 
       var col = 10;
@@ -24,15 +89,20 @@ $(document).ready(function()
         for(var colIdx=0; colIdx<col; colIdx++)
         {
 
-          //console.log(10*rowIdx+colIdx);
-
           if(10*rowIdx+1==colIdx)
           {
-            matrix[rowIdx].splice(colIdx);
+            matrix[rowIdx][colIdx]="Beep"
+          }
+          else if(10*rowIdx+2==colIdx)
+          {
+            matrix[rowIdx][colIdx]="Boop"
+          }
+          else if(10*rowIdx+3==colIdx)
+          {
+            matrix[rowIdx][colIdx =  "I'm sorry, Dave. I'm afraid I can't do that."
           }
           else
           {
-
             matrix[rowIdx][colIdx]=10*rowIdx +colIdx;
           }
           matrix[rowIdx][colIdx]=10*rowIdx +colIdx;
@@ -44,7 +114,7 @@ $(document).ready(function()
       }
 
       return matrix;
-    }
+    }*/
 
 
   });
