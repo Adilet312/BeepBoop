8991e59e7db222fd9d2b2b68f607132bedfc0765
Adilet Momunaliev <adilet@>
2019-11-01 14:13:06 -0700

    Filling two D array with elements,which was given by user

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,50 @@ $(document).ready(function()
   $("#formID").submit(function(event)
   {
       event.preventDefault();
-      var dataFromUser =$("#inputID").val();
-      console.log(dataFromUser);
+      var dataFromUser =parseFloat($("#inputID").val());
+      //console.log(dataFromUser);
+      //var size=Math.sqrt(dataFromUser);
+      var row = Math.ceil(dataFromUser/10);
+
+      var col = 10;
+      console.log(fullMatrix())
+
+
+
+ function fullMatrix()
+ {
+
+      var matrix= new Array()
+
+      for(var rowIdx=0;rowIdx<row; rowIdx++)
+      {
+        matrix[rowIdx] = new Array();
+        for(var colIdx=0; colIdx<col; colIdx++)
+        {
+
+          //console.log(10*rowIdx+colIdx);
+
+          if(10*rowIdx+1==colIdx)
+          {
+            matrix[rowIdx].splice(colIdx);
+          }
+          else
+          {
+
+            matrix[rowIdx][colIdx]=10*rowIdx +colIdx;
+          }
+          matrix[rowIdx][colIdx]=10*rowIdx +colIdx;
+          console.log(matrix[rowIdx][colIdx]);
+
+        }
+        console.log("  ");
+
+      }
+
+      return matrix;
+    }
+
+
   });
 
 });
