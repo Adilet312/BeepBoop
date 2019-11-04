3f7faa814b89d8c672400c84960adee38b353c62
Adilet312 <adiletm@hotmail.com>
2019-11-04 00:05:16 -0800

    Final version of project without README.

--- js/scripts.js
+++ js/scripts.js
@@ -3,110 +3,95 @@ $(document).ready(function()
     $("#formID").submit(function(event){
         event.preventDefault();
         var data = parseInt($("#textID").val());
-        console.log(data);
-        var matrix = [];
-        var index=data;
-        var stringV="";
-        var strinI="";
-        for(var idx=data; idx>=0; idx--)
-        {
-            stringV=idx.toString();
-            //console.log(stringV);
-            for(var innerIdx = 0; innerIdx<stringV.length; innerIdx+=2)
-            {
-                if((stringV[innerIdx]==='1'&& stringV[innerIdx+1]==='2') || (stringV[innerIdx]==='2'&& stringV[innerIdx+1]==='1'))
-                {
-                    stringV = spliceSlice(stringV,innerIdx,innerIdx+2,"boop");
-                    console.log(stringV,idx);
-                }
-                else if((stringV[innerIdx]==='1' && stringV[innerIdx+1]==='3') || (stringV[innerIdx]==='3' && stringV[innerIdx+1]==='1'))
-                {
-                    stringV = spliceSlice(stringV,innerIdx,innerIdx+2,"I'm sorry, Dave. I'm afraid I can't do that");
-                }
-                else if((stringV[innerIdx]==='2' && stringV[innerIdx+1]==='3')|| (stringV[innerIdx]==='3' && stringV[innerIdx+1]==='2'))
-                {
-                    stringV = spliceSlice(stringV,innerIdx,innerIdx+2,"I'm sorry, Dave. I'm afraid I can't do that");
-                }
-                else if((stringV[innerIdx]==='1' && stringV[innerIdx+1]!='2') && (stringV[innerIdx]==='1' && stringV[innerIdx+1]!='3'))
-                {
-                    stringV = spliceSlice(stringV,innerIdx,innerIdx+1,"beep");
-                }
-                else if((stringV[innerIdx+1]==='1' && stringV[innerIdx]!='2') && (stringV[innerIdx+1]==='1' && stringV[innerIdx]!='3'))
-                {
-                    stringV = spliceSlice(stringV,innerIdx+1,innerIdx+1,"beep");
-                }
-                else if((stringV[innerIdx]==='2' && stringV[innerIdx+1]!='1') && (stringV[innerIdx]==='2' && stringV[innerIdx+1]!='3'))
-                {
-                    stringV = spliceSlice(stringV,innerIdx,innerIdx+1,"boop");
-                }
-                else if((stringV[innerIdx+1]==='2' && stringV[innerIdx]!='1') && (stringV[innerIdx+1]==='2' && stringV[innerIdx]!='3'))
-                {
-                    stringV = spliceSlice(stringV,innerIdx+1,innerIdx+1,"boop");
-                }
-                else if((stringV[innerIdx]==='3' && stringV[innerIdx+1]!='1') && (stringV[innerIdx]==='3' && stringV[innerIdx+1]!='2'))
-                {
-                    stringV = spliceSlice(stringV,innerIdx,innerIdx+1,"I'm sorry, Dave. I'm afraid I can't do that");
-                }
-                else if((stringV[innerIdx+1]==='3' && stringV[innerIdx]!='1') && (stringV[innerIdx+1]==='3' && stringV[innerIdx]!='2'))
-                {
-                    stringV = spliceSlice(stringV,innerIdx+1,innerIdx+1,"I'm sorry, Dave. I'm afraid I can't do that");
-                }
-                /*
-                else if(stringV[innerIdx]==='1' && stringV[innerIdx+1]!='1' && stringV[innerIdx+1]!='2' && stringV[innerIdx+1]!='3')
-                {
-                   stringV = spliceSlice(stringV,innerIdx,innerIdx+1,"beep");
-                   console.log(stringV,idx);
-                }
-                else if(stringV[innerIdx]==='2' && stringV[innerIdx+1]!='2' && stringV[innerIdx+1]!='1' && stringV[innerIdx+1]!='3')
-                {
-                    stringV = spliceSlice(stringV,innerIdx,innerIdx+1,"boop"); 
-                    console.log(stringV,idx);
-                }
-                else if(stringV[innerIdx]==='3' && stringV[innerIdx+1]!='3' && stringV[innerIdx+1]!='1' && stringV[innerIdx+1]!='2')
-                {
-                    
-                    stringV = spliceSlice(stringV,innerIdx,innerIdx+1,"I'm sorry, Dave. I'm afraid I can't do that");
-                    console.log(stringV,idx);
-                }
-                else if(stringV[innerIdx]==='1' && stringV[innerIdx+1]==='1')
-                {
-                    
-                    stringV = spliceSlice(stringV,innerIdx,innerIdx+1,"beep");
-                    console.log(stringV,idx);
-                }
-                else if(stringV[innerIdx]==='2' && stringV[innerIdx+1]==='3')
-                {
-                    
-                    stringV = spliceSlice(stringV,innerIdx,innerIdx+1,"boop");
-                    console.log(stringV,idx);
-                }
-                else if(stringV[innerIdx]==='3' && stringV[innerIdx+1]==='3')
-                {
-                    
-                    stringV = spliceSlice(stringV,innerIdx,innerIdx+1,"I'm sorry, Dave. I'm afraid I can't do that.");
-                    console.log(stringV,idx);
-                }*/
+        var array = [];
+        var indexPosition=0;
+        var str ="";
+        for(var idx=0; idx<=data;idx++)
+        {   
+            
+            str = idx.toString();
+                for(var index=0; index<str.length; index+=2)
+                {   if(str[index]==='1'&& str[index+1]==='1'&& str[index+2]==='2')
+                    {
+                        str = spliceSlice(str,index,3,"beepboop");
+                    }
 
-            }
-            matrix[index]=stringV;
-            index--;
+                    else if(str[index]==='1'&& str[index+1]==='1'&& str[index+2]==='3')
+                    {
+                        str = spliceSlice(str,index,3,"beepI'm sorry, Dave. I'm afraid I can't do that");
+                    }
+                    else if(str[index]==='2'&& str[index+1]==='2'&& str[index+2]==='3')
+                    {
+                        str = spliceSlice(str,index,3,"boopI'm sorry, Dave. I'm afraid I can't do that");
+                    }
+                    // Replace "12" or"21" if they in row by word "boop".
+                    else if((str[index]==='1'&& str[index+1]==='2') ||
+                       (str[index]==='2'&& str[index+1]==='1'))
+                    {
+                        str = spliceSlice(str,index,2,"boop");
+                    }
+                    // Replace "13" or"31" if they in row by word "I'm sorry, Dave. I'm afraid I can't do that".
+                    else if((str[index]==='1' && str[index+1]==='3') ||
+                            (str[index]==='3' && str[index+1]==='1'))
+                    {
+                        str = spliceSlice(str,index,2,"I'm sorry, Dave. I'm afraid I can't do that");
+                    }
+                    // Replace "23" or"32" if they in row by word "I'm sorry, Dave. I'm afraid I can't do that".
+                    else if((str[index]==='2' && str[index+1]==='3')||
+                            (str[index]==='3' && str[index+1]==='2'))
+                    {
+                        str = spliceSlice(str,index,2,"I'm sorry, Dave. I'm afraid I can't do that");
+                    }
+                    //Replace "1" by word "beep"
+                    else if(str[index]==='1' && str[index+1]==='1')
+                    {
+                        str = spliceSlice(str,index,2,"beepbeep");
+                    }
+                    else if(str[index]==='1' && str[index+1]!='1')
+                    {
+                        str = spliceSlice(str,index,1,"beep");
+                    }
+                    else if(str[index+1]==='1'&& str[index]!='1')
+                    {
+                        str =spliceSlice(str,index+1,1,"beep")
+                    }
+                    //Replace "2" by word "boop"
+                    if(str[index]==='2' && str[index+1]==='2')
+                    {
+                        str = spliceSlice(str,index,2,"boopboop");
+                    }
+                    else if(str[index]==='2' && str[index+1]!='2')
+                    {
+                        str = spliceSlice(str,index,1,"boop");
+                    }
+                    else if(str[index+1]==='2'&& str[index]!='2')
+                    {
+                        str =spliceSlice(str,index+1,1,"boop")
+                    }
+                    //Replace "3" by word "boop"
+                    if(str[index]==='3' && str[index+1]==='3')
+                    {
+                        str = spliceSlice(str,index,2,"I'm sorry, Dave. I'm afraid I can't do that I'm sorry, Dave. I'm afraid I can't do that");
+                    }
+                    else if(str[index]==='3' && str[index+1]!='3')
+                    {
+                        str = spliceSlice(str,index,1,"I'm sorry, Dave. I'm afraid I can't do that");
+                    }
+                    else if(str[index+1]==='3'&& str[index]!='3')
+                    {
+                        str =spliceSlice(str,index+1,1,"I'm sorry, Dave. I'm afraid I can't do that")
+                    }
+                    
+                }
+                array[indexPosition] = str;
+                console.log(array[indexPosition]);
+                indexPosition++;
+                
+                
+            
         }
-        console.log(matrix);
-
-       
-        
-
-
-
-
-
-
-
-
-
-
-
-
+        //Function was copied below link (splice() function doesn't support string and it works only with arrray, so below function was developed to work with string)
+       //https://stackoverflow.com/questions/20817618/is-there-a-splice-method-for-strings##targetText=No%2C%20there%20is%20no%20such,the%20string%20into%20an%20array.
         function spliceSlice(str, index, count, add) 
         {
             
@@ -121,6 +106,7 @@ $(document).ready(function()
           
             return str.slice(0, index) + (add || "") + str.slice(index + count);
           }
+          $("#output").text(array);
     });
 
 });
